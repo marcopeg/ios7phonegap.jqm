53f3ebaadec947e79f1281c8128ac2c391bce5c7
thepeg <marco.pegoraro@gmail.com>
2014-01-03 18:04:23 +0100

    first commit

---
+++ ios7phonegap.jqm.js
@@ -0,0 +1,35 @@
+/**
+ * Shopping List
+ * v1.0.0
+ */
+
+;(function($) {
+	
+	
+	
+	$(document).on("pageinit", function( event ) {
+		if (navigator.userAgent.match(/(iPad|iPhone);.*CPU.*OS 7_\d/i)) {
+			
+			/**
+			 * allow to inherit color from a custom setting
+			 */
+			var color = window.ios7statusbarColor || 'rgb(233,233,233)';
+			
+			/**
+			 * create the custom style node 
+			 */
+			$('<style>').html(''
+				+ '#ios7phonegap {width:100%;height:20px;position:fixed;z-index:100;background:' + color + '}'
+				+ '.ios7phonegap .ui-page {margin-top: 20px}'
+				+ '.ios7phonegap .ui-header {border-top-color:' + color + ' !important}'
+			).appendTo($('head'));
+			
+			/**
+			 * apply DOM transformations
+			 */
+			$("body").addClass("ios7phonegap").append('<div id="ios7phonegap"/>');
+			
+		}
+	});
+	
+})(jQuery);
---
+++ readme.md
@@ -0,0 +1,15 @@
+ios7phonegap.jqm | jQueryMobile Plugin
+======================================
+
+this plugin fix a very odd behavior of _jQueryMobile_ when runned under _iOS7_ with _PhoneGap_.
+
+## Instructions
+
+Just drop the plugin file after the `jquery.js` script.
+
+## Notes
+
+**fixed toolbar and footer** are yet to be tested!
+
+You'd better use it in combination with 
+[iScrollPage](https://github.com/movableapp/iScrollPage).
